03e364c6e8b8c7fd2718a61a8812e9d915b641ff
SEPEHR <sep7696@gmail.com>
2024-01-08 11:41:27 +0330

    Delete LeaveType command and han lder is Initlized.

---
+++ HR.Managment.Application/Features/LeaveTypes/Commands/DeleteLeaveType/DeleteLeaveTypeCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Managment.Application.Features.LeaveTypes.Commands.DeleteLeaveType
+{
+    public class DeleteLeaveTypeCommand : IRequest<Unit>
+    {
+        public int LeaveTypeID { get; set; }
+    }
+}
---
+++ HR.Managment.Application/Features/LeaveTypes/Commands/DeleteLeaveType/DeleteLeaveTypeCommandhandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HR.Managment.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Managment.Application.Features.LeaveTypes.Commands.DeleteLeaveType
+{
+    public class DeleteLeaveTypeCommandhandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveTypeCommandhandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveTypetoDelete = await _leaveTypeRepository.GetByIDAsync(request.LeaveTypeID);
+            if(leaveTypetoDelete is null ) 
+                throw new NullReferenceException(nameof(leaveTypetoDelete));
+            await _leaveTypeRepository.DeleteAsync(request.LeaveTypeID);
+            return Unit.Value;
+        }
+    }
+}
