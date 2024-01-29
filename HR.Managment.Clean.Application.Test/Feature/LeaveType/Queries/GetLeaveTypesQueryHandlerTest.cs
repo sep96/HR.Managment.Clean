7d0b8a9e24ff9ad41e2daab77def3d75a5fc2454
SEPEHR <sep7696@gmail.com>
2024-01-29 14:36:36 +0330

    Test : Add GetLEaveTypeMockLeaveTypeRepository and  GetLeaveTypesQueryHandlerTest

---
+++ HR.Managment.Clean.Application.Test/Feature/LeaveType/Queries/GetLeaveTypesQueryHandlerTest.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using HR.Managment.Application.Contracts.Persistence;
+using HR.Managment.Application.Features.LeaveTypes.Queries.GetAllLeaveTypes;
+using HR.Managment.Application.Features.LeaveTypes.Queries.GetLeaveTypeDetails;
+using HR.Managment.Application.MappingProfiles;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Managment.Clean.Application.Test.Feature.LeaveType.Queries
+{
+    public class GetLeaveTypesQueryHandlerTest
+    {
+        private readonly Mock<ILeaveTypeRepository> _mock;
+        private IMapper _mapper;
+        public GetLeaveTypesQueryHandlerTest()
+        {
+            _mock = MoqLeaveTypeRepository.GetLEaveTypeMockLeaveTypeRepository();
+            var mapConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<LeaveTypeProfile>();
+            });
+            _mapper = mapConfig.CreateMapper(); 
+        }
+       [Fact]
+       public async Task GetLeaveTypeListTest()
+       {
+            var hanlder = new GetLeaveTypesQueryhandler(_mock.Object , _mapper);
+            var result = await hanlder.Handle(new GetLeaveTypesQuery(), CancellationToken.None);
+            result.ShouldBeOfType<List<LeaveTypesDto>>();
+            result.Count.ShouldBe(4);   
+       }
+
+    
+    }
+}
---
+++ HR.Managment.Clean.Application.Test/Moqs/MoqLeaveTypeRepository.cs
@@ -0,0 +1,54 @@
+﻿using HR.Managment.Application.Contracts.Persistence;
+using HR.Managment.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Managment.Clean.Application.Test.Feature.LeaveType
+{
+    public class MoqLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetLEaveTypeMockLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveTypes>
+            {
+                new LeaveTypes
+                {
+                    LeaveTypesID =1 ,
+                    LeaveTypesName = "Test1" ,
+                    LeaveTypesDefaultDays = 1
+                },
+                new LeaveTypes
+                {
+                    LeaveTypesID =2 ,
+                    LeaveTypesName = "Test2" ,
+                    LeaveTypesDefaultDays = 2
+                },
+                new LeaveTypes
+                {
+                    LeaveTypesID =3 ,
+                    LeaveTypesName = "Test3" ,
+                    LeaveTypesDefaultDays = 3
+                },
+                new LeaveTypes
+                {
+                    LeaveTypesID =4 ,
+                    LeaveTypesName = "Test4" ,
+                    LeaveTypesDefaultDays = 4
+                }
+            };
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+            mockRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(leaveTypes);
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<LeaveTypes>())).ReturnsAsync
+                ((LeaveTypes leaveType) =>
+                {
+                    leaveTypes.Add(leaveType);
+                    return leaveType;
+                });
+            return mockRepo;
+        }
+    }
+}
