bfb7ccc82c7669602e49b92e2322630f4430a06d
SEPEHR <sep7696@gmail.com>
2024-01-31 10:23:40 +0330

    Feat: Add HrLeaveManagmentIdentityDbContext.

---
+++ HR.Managment.Clean.Identity/DbContext/HrLeaveManagmentIdentityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using HR.Managment.Clean.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Managment.Clean.Identity.DbContext
+{
+    public class HrLeaveManagmentIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public HrLeaveManagmentIdentityDbContext(DbContextOptions<HrLeaveManagmentIdentityDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(HrLeaveManagmentIdentityDbContext).Assembly);
+        }
+    }
+}
